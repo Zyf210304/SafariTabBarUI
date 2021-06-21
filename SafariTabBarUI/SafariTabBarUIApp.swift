ee91c26ab8bcf15b3b505ce47a5e7652581d5cae
张亚飞 <672233580@qq.com>
2021-06-21 09:34:30 +0800

    Initial Commit

---
+++ SafariTabBarUI/SafariTabBarUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariTabBarUIApp.swift
+//  SafariTabBarUI
+//
+//  Created by 张亚飞 on 2021/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SafariTabBarUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
